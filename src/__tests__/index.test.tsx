3b3561f0bf0817fee6376097800de40dc3e18ca9
Josh Ellis <joshua.ellis18@gmail.com>
2021-03-11 09:48:16 +0000

    refactor: move to use rttr

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,51 +1,23 @@
 import * as React from 'react'
-import { useThree } from 'react-three-fiber'
 import { Vector3 } from 'three'
 
 import { renderScene } from '../index'
 
 describe('react-three-testing-library', () => {
-  // it("should render a scene", () => {
-  //   const position = new Vector3(1, 1, 1)
+  it('should render a scene', () => {
+    const position = new Vector3(1, 1, 1)
 
-  //   const { scene } = renderScene(
-  //     <mesh position={position}>
-  //       <boxBufferGeometry args={[1, 1, 1]} />
-  //       <meshBasicMaterial />
-  //       <mesh>
-  //         <boxBufferGeometry args={[1, 1, 1]} />
-  //         <meshBasicMaterial userData={{
-  //           testId: 'material'
-  //         }} />
-  //       </mesh>
-  //     </mesh>
-  //   )
-
-  //   expect(scene).toBeTruthy()
-  // })
-
-  const TestComponent = () => {
-    const geomRef = React.useRef<THREE.BoxBufferGeometry>(null!)
-    const handlePointerDown = () => {
-      geomRef.current.scale(10, 10, 10)
-    }
-    return (
-      <mesh onPointerDown={handlePointerDown} userData={{ testId: 'mesh' }}>
-        <boxBufferGeometry ref={geomRef} args={[1, 1, 1]} />
+    const { scene } = renderScene(
+      <mesh position={position}>
+        <boxBufferGeometry args={[1, 1, 1]} />
         <meshBasicMaterial />
+        <mesh>
+          <boxBufferGeometry args={[1, 1, 1]} />
+          <meshBasicMaterial />
+        </mesh>
       </mesh>
     )
-  }
 
-  it('should handle a pointerDown event', () => {
-    const { getByTestId } = renderScene(<TestComponent />)
-
-    const el = getByTestId('mesh')
-    console.log(el)
-    el.props.onPointerDown()
-
-    console.log(getByTestId('geom'))
-
-    expect(getByTestId('geom')).toBeFalsy()
+    expect(scene).toBeTruthy()
   })
 })
--- src/core/index.ts
+++ src/core/index.ts
@@ -1,60 +1,8 @@
-import React from 'react'
-import TestRenderer, { ReactTestRenderer } from 'react-test-renderer'
+import * as React from 'react'
+import ReactThreeTestRenderer from 'react-three-test-renderer'
 
-import { getQueriesForScene } from '../helpers/getQueriesForScene'
-
-import {
-  RenderSceneOptions,
-  RenderSceneResult,
-} from '../types'
-import {TestHarnessCanvasProps} from '../types/internal'
-
-import { testHarness } from './testHarness'
-
-import createContext from 'gl'
-
-const renderScene = (
-  scene: React.ReactElement,
-  { canvasProps }: RenderSceneOptions = {}
-) => {
-  const renderer = renderWithAct(scene, canvasProps)
-
-  const renderedScene = renderer.root
-
-  // console.log(getQueriesForScene(renderedScene))
-
-  return {
-    scene: renderedScene,
-    unmount: () => renderer!.unmount(),
-    rerender: (renderScene: React.ReactElement) => {
-      TestRenderer.act(() => {
-        renderer!.update(renderScene)
-      })
-    },
-    ...getQueriesForScene(renderedScene)
-  } as RenderSceneResult
-}
-
-const renderWithAct = (
-  scene: React.ReactElement,
-  canvasProps: TestHarnessCanvasProps = {}
-): ReactTestRenderer => {
-  let renderer: ReactTestRenderer = TestRenderer.create(testHarness(scene, canvasProps), {
-    createNodeMock: (el) => ({
-      ...el,
-      width: 1280,
-      height: 800,
-      style: {},
-      getContext: () => {
-        const context = createContext(1280, 800)
-        return context
-      },
-      addEventListener: () => null,
-      removeEventListener: () => null
-    })
-  })
-
-  return (renderer as unknown) as ReactTestRenderer
+const renderScene = (el: React.ReactNode) => {
+  return ReactThreeTestRenderer.create(el)
 }
 
 export { renderScene }
